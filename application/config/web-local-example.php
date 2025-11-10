2ad358fe4a59052a3529687c8200e47c4a532820
user <aroslavakorobova89540@gmail.comgit config --global user.name eXp4ndingUniverse>
2025-11-10 20:55:41 +0300

    Update web-local-example.php with actual configuration

--- application/config/web-local-example.php
+++ application/config/web-local-example.php
@@ -1,18 +1,44 @@
 <?php
 /**
- * [Образец] файла для локального перопределения
- * (миниум того, что скорее всего придётся переопредлить)
+ * Конфигурационной файл приложения
  */
-
 $config = [
     'core' => [ // подмассив используемый самим ядром фреймворка
-        'db' => [ // подмассив конфигурации БД
+        'db' => [
             'dns' => 'mysql:host=localhost;dbname=smvcbase',
             'username' => 'root',
             'password' => '1234'
+        ],
+        'router' => [ // подсистема маршрутизация
+            'class' => \ItForFree\SimpleMVC\Router\WebRouter::class,
+	    'alias' => '@router'
+        ],
+        'mvc' => [ // настройки MVC
+            'views' => [
+                'base-template-path' => '../application/views/',
+                'base-layouts-path' => '../application/views/layouts/',
+                'footer-path' => '',
+                'header-path' => ''
+            ]
+        ],
+        'handlers' => [ // подсистема перехвата исключений
+            'ItForFree\SimpleMVC\Exceptions\SmvcAccessException' 
+		=> \application\handlers\UserExceptionHandler::class,
+            'ItForFree\SimpleMVC\Exceptions\SmvcRoutingException' 
+		=> \application\handlers\UserExceptionHandler::class
+        ],
+        'user' => [ // подсистема авторизации
+            'class' => \application\models\AuthUser::class,
+	    'construct' => [
+                'session' => '@session',
+                'router' => '@router'
+             ], 
+        ],
+        'session' => [ // подсистема работы с сессиями
+            'class' => ItForFree\SimpleMVC\Session::class,
+            'alias' => '@session'
         ]
     ]    
 ];
-
 
 return $config;
